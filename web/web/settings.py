4a6ac801a61c1e71a2ec76fb267b261a54e96732
zhenya194 <zhenya.pro1753@gmail.com>
2026-05-31 09:12:01 +0300

    Fix repeated parts of .css files in lessons and articles apps

--- web/web/settings.py
+++ web/web/settings.py
@@ -109,6 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 LOGIN_REDIRECT_URL = '/profile/'
 
