043484a87ca4c647b5216a48448a24cdff69b206
zhenya194 <zhenya.pro1753@gmail.com>
2026-06-01 10:43:32 +0300

    Create lessons and articles count on main page

--- web/apps/main/views.py
+++ web/apps/main/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..lessons.models import Lesson
+from ..articles.models import Article
 
 def index(request):
-    return render(request, 'main/index.html')
+    lessons_count = Lesson.objects.all().count() - 11
+    articles_count = Article.objects.all().count() - 11
+    return render(
+        request, "main/index.html",
+        {
+            "lessons_count": lessons_count,
+            "articles_count": articles_count
+        }
+    )
 
 def privacy(request):
-    return render(request, 'main/privacy.html')
+    return render(request, "main/privacy.html")
