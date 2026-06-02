b3308b0e43edfb4341a7ff97576db702de8632a8
zhenya194 <zhenya.pro1753@gmail.com>
2026-06-02 12:17:31 +0300

    Update main views

--- web/apps/main/views.py
+++ web/apps/main/views.py
@@ -4,8 +4,8 @@ from ..lessons.models import Lesson
 from ..articles.models import Article
 
 def index(request):
-    lessons_count = Lesson.objects.all().count() - 11
-    articles_count = Article.objects.all().count() - 11
+    lessons_count = Lesson.objects.filter(status="approved").count() - 11
+    articles_count = Article.objects.filter(status="approved").count() - 11
     return render(
         request, "main/index.html",
         {
