7d6210c26114d368479c8533bf2479da353bb190
zhenya194 <zhenya.pro1753@gmail.com>
2026-06-02 17:56:46 +0300

    Update olympiad page

--- web/apps/olympiads/static/olympiads/olympiad.js
+++ web/apps/olympiads/static/olympiads/olympiad.js
@@ -1,4 +1,6 @@
 const ask = document.getElementById("ask");
+const process_olympiad_tasks = document.getElementById("process_olympiad");
 function yes() {
     ask.style.display = "none";
+    process_olympiad_tasks.style.display = "block";
 }
