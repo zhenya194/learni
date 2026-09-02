e30a512f328791f1e61976e67f2fdad5be32c140
zhenya194 <zhenya.pro1753@gmail.com>
2026-09-02 11:17:29 +0300

    Update user model

--- web/apps/profile/models.py
+++ web/apps/profile/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ..olympiads.models import Olympiad
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(unique=True)
+    completed_olympiads = models.ManyToManyField(Olympiad, related_name="completed_olympiads", blank=True)
